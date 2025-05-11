c68d029f6a44ce1c8de8920197452d423fc7ac1d
iibrahimjrr <ibrahim22al5@gmail.com>
2025-05-11 06:52:13 -0700

    finally updates

--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -49,7 +49,7 @@ const Register = () => {
     }
 
     try {
-      const response = await axios.post('http://127.0.0.1:8000/api/register', {
+      const response = await axios.any('http://127.0.0.1:8000/api/register', {
         name,
         username,
         email,
